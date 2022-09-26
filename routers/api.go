ade37094e6bdfeb44210af1601b35539415611b6
RockRockWhite <1834309151@qq.com>
2022-09-26 18:51:34 +0800

    fix bind /

--- routers/api.go
+++ routers/api.go
@@ -23,7 +23,7 @@ func InitApiRouter() *gin.Engine {
 	user := router.Group("/users")
 	{
 		user.GET("/:username", controllers.GetUser)
-		user.POST("/", controllers.AddUser)
+		user.POST("", controllers.AddUser)
 		user.PATCH(
 			"/:username",
 			middlewares.JwtAuth(middlewares.Role_Admin|middlewares.Role_Cond, func(c *gin.Context) bool {
@@ -47,8 +47,8 @@ func InitApiRouter() *gin.Engine {
 	paper := router.Group("/papers")
 	{
 		paper.GET("/:id", controllers.GetPaper)
-		paper.GET("/", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.GetPapers)
-		paper.POST("/", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.AddPaper)
+		paper.GET("", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.GetPapers)
+		paper.POST("", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.AddPaper)
 		paper.PATCH("/:id", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.PatchPaper)
 		paper.DELETE("/:id", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.DeletePaper)
 	}
@@ -56,8 +56,8 @@ func InitApiRouter() *gin.Engine {
 	teacher := router.Group("/teachers")
 	{
 		teacher.GET("/:id", controllers.GetTeacher)
-		teacher.GET("/", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.GetTeachers)
-		teacher.POST("/", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.AddTeacher)
+		teacher.GET("", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.GetTeachers)
+		teacher.POST("", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.AddTeacher)
 		teacher.PATCH("/:id", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.PatchTeacher)
 		teacher.DELETE("/:id", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.DeleteTeacher)
 	}
@@ -65,8 +65,8 @@ func InitApiRouter() *gin.Engine {
 	news := router.Group("/news")
 	{
 		news.GET("/:id", controllers.GetNews)
-		news.GET("/", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.GetNewsList)
-		news.POST("/", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.AddNews)
+		news.GET("", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.GetNewsList)
+		news.POST("", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.AddNews)
 		news.PATCH("/:id", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.PatchNews)
 		news.DELETE("/:id", middlewares.JwtAuth(middlewares.Role_Admin, nil), controllers.DeleteNews)
 	}
