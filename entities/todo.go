ecfa2c6ebfd91258561322e7c8cb878dd6468ccf
RockRockWhite <1834309151@qq.com>
2022-10-11 17:24:59 +0800

    created: entity for todo list

---
+++ entities/todo.go
@@ -0,0 +1,20 @@
+package entities
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+// Todo Todo表实体类
+type Todo struct {
+	gorm.Model
+	Title    string    `gorm:"not null"` // Todo表标题
+	From     time.Time `gorm:"not null"` // 开始时间
+	To       time.Time `gorm:"not null"` // 完成时间
+	Finished bool      // 完成状态
+	UserId   uint      `gorm:"not null"` // 创建用户Id
+}
+
+func (Todo) TableName() string {
+	return "t_todos"
+}
